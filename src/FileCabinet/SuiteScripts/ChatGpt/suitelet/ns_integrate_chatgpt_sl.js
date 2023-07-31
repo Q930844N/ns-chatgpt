0eddfe8bb00e0b8df368bec473d3c5f3add8d5aa
abdulqadeer <abdulqadeer@amyris.com>
2023-07-31 20:05:20 +0500

    updated with suiteql process

--- src/FileCabinet/SuiteScripts/ChatGpt/suitelet/ns_integrate_chatgpt_sl.js
+++ src/FileCabinet/SuiteScripts/ChatGpt/suitelet/ns_integrate_chatgpt_sl.js
@@ -3,14 +3,16 @@
 * @NScriptType Suitelet
 * @NModuleScope Public
 * 
-* Author: Abdul Qadeer with the help og ChatGPT.
+* Author: Abdul Qadeer with the help of ChatGPT.
 */
 
-define(['N/ui/serverWidget', 'N/https', 'N/url', 'N/redirect'], function (serverWidget, https, url, redirect) {
+// write a suitescript 2.1 for aftersubmit to update memo field in netsuite
+
+define(['N/ui/serverWidget', 'N/https', 'N/url', 'N/redirect', 'N/query'], function (serverWidget, https, url, redirect, query) {
 	function onRequest(context) {
 		log.debug('context', context);
 		// Get the user input from the form submission
-		var userInput = context.request.parameters?.custpage_query_text;
+		var userInput = context.request.parameters?.custpage_query_text || '';
 		log.debug('userInput', userInput);
 
 		if (context.request.method === 'GET') {
@@ -19,19 +21,49 @@ define(['N/ui/serverWidget', 'N/https', 'N/url', 'N/redirect'], function (server
 				title: 'Prompt ChatGPT',
 			});
 
+			form.addFieldGroup({
+				id: "custpage_nschatgpt_question",		
+				label: "Prompt",
+			  });
+
 			// Add a textarea field for user input
 			form.addField({
 				id: 'custpage_query_text',
 				label: 'Enter Text',
 				type: serverWidget.FieldType.TEXTAREA,
-			});
+				container: 'custpage_nschatgpt_question',
+			}).defaultValue = userInput;
 			if (userInput) {
+				form.addFieldGroup({
+					id: "custpage_nschatgpt_answer",		
+					label: "ChatGPT Response",
+				  });
 				var convertedQuery = getConvertedQueryFromOpenAI(userInput);
 				form.addField({
 					id: 'custpage_query_results',
 					label: 'Converted Query:',
 					type: serverWidget.FieldType.INLINEHTML,
+					container: 'custpage_nschatgpt_answer',
 				}).defaultValue = '<pre>' + convertedQuery + '</pre>';
+				log.debug('convertedQuery', convertedQuery);
+				if(convertedQuery){
+					convertedQuery = JSON.parse(convertedQuery);
+					var choices = convertedQuery["choices"];
+					log.debug('choices', choices);
+					var sql = choices[0].message.content;
+					log.debug('sql', sql);
+					form.addFieldGroup({
+						id: "custpage_nschatgpt_query_res",		
+						label: "Query Response",
+					  });
+					var suiteqlResponse = getSuiteQLRespponse(sql);
+					form.addField({
+						id: 'custpage_suiteql_results',
+						label: 'Query Results:',
+						type: serverWidget.FieldType.INLINEHTML,
+						container: 'custpage_nschatgpt_query_res',
+					}).defaultValue = '<pre>' + JSON.stringify(suiteqlResponse) + '</pre>';
+				}
 			}
 
 			// Add a submit button
@@ -47,7 +79,7 @@ define(['N/ui/serverWidget', 'N/https', 'N/url', 'N/redirect'], function (server
 			var suiteletURL = url.resolveScript({
 				scriptId: 'customscript_ns_chatgpt_sl',
 				deploymentId: 'customdeploy_ns_chatgpt_sl',
-				params: {custpage_query_text : context.request.parameters.custpage_query_text}
+				params: { custpage_query_text: context.request.parameters.custpage_query_text }
 			});
 			redirect.redirect({ url: suiteletURL });
 		}
@@ -58,7 +90,7 @@ define(['N/ui/serverWidget', 'N/https', 'N/url', 'N/redirect'], function (server
 		// Call the OpenAI API with the input text and obtain the converted query
 		// (Replace the following with actual API integration code)
 		var apiUrl = 'https://api.openai.com/v1/chat/completions';
-		var apiKey = 'PLACE_YOUR_API_KEY_HERE'; // Replace with your OpenAI API key
+		var apiKey = 'PLACE_API_KEY_HERE'; // Replace with your OpenAI API key
 		var headers = {
 			'Authorization': 'Bearer ' + apiKey,
 			'Content-Type': 'application/json',
@@ -81,6 +113,17 @@ define(['N/ui/serverWidget', 'N/https', 'N/url', 'N/redirect'], function (server
 			response = error;
 		}
 		return response.body;
+	}
+
+	// call SuiteQL
+	function getSuiteQLRespponse(qry) {
+		var resut;
+		try {
+			resut = query.runSuiteQL(qry).asMappedResults();
+		} catch (error) {
+			resut = error
+		}
+		return resut;
 	}
 
 	return {
